93646f4f57f16f5b8936dbd79217f880c8417d79
Bernardo López <bernardolopezf.dev@gmail.com>
2025-02-28 19:29:43 -0500

    Add: header, css. Edited: .gitignore and README

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+export function Buttons({button_name}) {
+    return (
+        <button className="HButtons">
+            {button_name}
+        </button>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import React from 'react'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+import { Buttons } from './Components/Header.jsx'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <div className="header-button-container">
+  <Buttons button_name="Home" />
+  <Buttons button_name="News" />
+  <Buttons button_name="Characters" />
+  <Buttons button_name="Lore" />  
+  <Buttons button_name="Regions"/>
+  <Buttons button_name="TBD"/>
+  </div>,
 )
+  
+
